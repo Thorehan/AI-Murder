ac0600663df78928ba9040cae69d0e3dd3be54a9
ozn1911 <117126115+ozn1911@users.noreply.github.com>
2025-05-28 11:24:03 +0300

    some things

--- Assets/Scripts/NPCWorker.cs
+++ Assets/Scripts/NPCWorker.cs
@@ -2,9 +2,12 @@ using UnityEngine;
 using UnityEngine.AI;
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine.Networking;
 
 public class NPCWorker : MonoBehaviour
 {
+    public string npcName = "Worker";
     public float taskDuration = 3f;
     public float minDistanceToOther = 2f;
 
@@ -14,6 +17,22 @@ public class NPCWorker : MonoBehaviour
     private Transform currentTarget;
     private NavMeshAgent agent;
     private bool isDoingTask = false;
+
+    Dictionary<NPCWorker, Cooldown> cooldowns = new Dictionary<NPCWorker, Cooldown>();
+
+    Dictionary<string, Transform> TaskRooms = new Dictionary<string, Transform>();
+    public void AddTaskRoomsFromList(List<Transform> roomList)
+    {
+        foreach (Transform room in roomList)
+        {
+            if (room != null && !TaskRooms.ContainsKey(room.name))
+            {
+                string temp = room.name.Replace(" ", "");
+                TaskRooms.Add(temp, room);
+            }
+        }
+    }
+
 
     //public Transform sleepPoint;
 
@@ -32,8 +51,8 @@ public class NPCWorker : MonoBehaviour
             Debug.LogError("No task points");
             return;
         }
-
-        PickNewTarget();
+        AddTaskRoomsFromList(taskPoints);
+        StartCoroutine(SendSignalToLocalhost("Game Start"));
     }
 
     void Update()
@@ -49,7 +68,8 @@ public class NPCWorker : MonoBehaviour
                 float distance = Vector3.Distance(transform.position, other.transform.position);
                 if (distance < minDistanceToOther)
                 {
-                    PickNewTarget();
+                    //StartCoroutine(SendSignalToLocalhost("TooCloseToOtherNPC"));
+                    //PickNewTarget();
                     return;
                 }
             }
@@ -75,7 +95,66 @@ public class NPCWorker : MonoBehaviour
         isDoingTask = true;
         yield return new WaitForSeconds(taskDuration);
         isDoingTask = false;
-        PickNewTarget();
+        //PickNewTarget();
+        SendSignalToLocalhost("TaskCompleted");
+    }
+
+
+
+    public void OnSignal(string signal)
+    {
+        // Send API call to localhost with the signal
+        StartCoroutine(SendSignalToLocalhost(signal));
+    }
+
+    IEnumerator SendSignalToLocalhost(string signal)
+    {
+        Debug.Log($"Sending signal: {signal}");
+        string temp = npcName.Replace(" ", "");
+        string url = $"http://localhost:8000/{temp}?prompt={UnityWebRequest.EscapeURL(signal)}";
+        using (UnityWebRequest www = UnityWebRequest.Get(url))
+        {
+            yield return www.SendWebRequest();
+
+            if (www.result != UnityWebRequest.Result.Success)
+            {
+                //Debug.LogError($"Error sending signal: {www.error}");
+            }
+            else
+            {
+                Debug.Log($"{www.downloadHandler.text}");
+                if (www.downloadHandler.text.Contains("<DinnerRoom>"))
+                {
+                    Transform newTarget = TaskRooms["DinnerRoom"];
+                    currentTarget = newTarget;
+                    agent.SetDestination(currentTarget.position);
+                }
+                else if (www.downloadHandler.text.Contains("<Kicten>"))
+                {
+                    Transform newTarget = TaskRooms["Kicten"];
+                    currentTarget = newTarget;
+                    agent.SetDestination(currentTarget.position);
+                }
+                else if (www.downloadHandler.text.Contains("<TeaRoom>"))
+                {
+                    Transform newTarget = TaskRooms["TeaRoom"];
+                    currentTarget = newTarget;
+                    agent.SetDestination(currentTarget.position);
+                }
+                else if (www.downloadHandler.text.Contains("<Garden>"))
+                {
+                    Transform newTarget = TaskRooms["Garden"];
+                    currentTarget = newTarget;
+                    agent.SetDestination(currentTarget.position);
+                }
+                else if (www.downloadHandler.text.Contains("<Corridor>"))
+                {
+                    Transform newTarget = TaskRooms["Corridor"];
+                    currentTarget = newTarget;
+                    agent.SetDestination(currentTarget.position);
+                }
+            }
+        }
     }
 
     void OnDestroy()
